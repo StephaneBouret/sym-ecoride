a0d7999a1c2dba35261fccc97232404a4388d86b
StephaneBouret <59822988+StephaneBouret@users.noreply.github.com>
2025-06-06 19:08:15 +0200

    Réinitialisation de mot de passe

---
+++ migrations/Version20250606152555.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250606152555 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user ADD reset_token VARCHAR(255) DEFAULT NULL, ADD created_token_at DATETIME DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user DROP reset_token, DROP created_token_at
+        SQL);
+    }
+}
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ResetPasswordFormTypeForm;
+use DateTimeImmutable;
+use App\Repository\UserRepository;
+use App\Service\PasswordResetService;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ResetPasswordRequestFormTypeForm;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -28,5 +36,64 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route('/forgot-password', name: 'app_forgot_pw')]
+    public function forgotPw(Request $request, UserRepository $userRepository, PasswordResetService $passwordResetService): Response
+    {
+        $form = $this->createForm(ResetPasswordRequestFormTypeForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $userRepository->findOneBy([
+                'email' => $form->get('email')->getData()
+            ]);
+
+            if ($user) {
+                $passwordResetService->processPasswordReset($user);
+                $this->addFlash('success', 'Email envoyé avec succès');
+                return $this->redirectToRoute('app_login');
+            }
+            // $user est null
+            $this->addFlash('danger', 'Un problème est survenu');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password_request.html.twig', [
+            'formView' => $form,
+        ]);
+    }
+
+    #[Route('/forgot-password/{token}', name: 'app_reset_pw')]
+    public function resetPw($token, Request $request, UserRepository $userRepository, EntityManagerInterface $em, UserPasswordHasherInterface $hasher, PasswordResetService $passwordResetService): Response
+    {
+        // On vérifie si on a ce token dans la bdd
+        $user = $passwordResetService->getUserByResetToken($token, $userRepository);
+
+        // Si le token est invalide on redirige vers le login
+        if (!$user) {
+            $this->addFlash('danger', 'Jeton invalide');
+            return $this->redirectToRoute('app_login');
+        }
+
+        // On vérifie si le createdTokenAt = now - 1h
+        if ($passwordResetService->isTokenExpired($user)) {
+            $this->addFlash('warning', 'Votre demande de mot de passe a expiré. Merci de la renouveller.');
+            return $this->redirectToRoute('app_forgot_pw');
+        }
+
+        // On modifie le mot de passe
+        $form = $this->createForm(ResetPasswordFormTypeForm::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $passwordResetService->updatePassword($user, $form->get('plainPassword')->getData(), $hasher);
+            $this->addFlash('success', 'Mot de passe changé avec succès');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password.html.twig', [
+            'passForm' => $form
+        ]);
     }
 }
