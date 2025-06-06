4f18353763a914934c1da420132c0298cb9a3036
StephaneBouret <59822988+StephaneBouret@users.noreply.github.com>
2025-06-06 16:50:04 +0200

    Mise en place de l'authenticator et du formulaire de connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        $this->addFlash('success', 'Bienvenue sur ton application');
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
