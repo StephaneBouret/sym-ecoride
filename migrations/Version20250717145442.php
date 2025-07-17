f8f2385a39b3430ff903a74fdae1c94488ee47e4
StephaneBouret <59822988+StephaneBouret@users.noreply.github.com>
2025-07-17 17:46:26 +0200

    Mise en place de l'entité Testimonial et de la méthode de création ainsi que du controller stimulus

---
+++ migrations/Version20250717145442.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250717145442 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE testimonial (id INT AUTO_INCREMENT NOT NULL, trip_id INT NOT NULL, author_id INT NOT NULL, rating INT NOT NULL, content LONGTEXT NOT NULL, is_approved TINYINT(1) DEFAULT 0 NOT NULL, created_at DATETIME NOT NULL COMMENT '(DC2Type:datetime_immutable)', INDEX IDX_E6BDCDF7A5BC2E0E (trip_id), INDEX IDX_E6BDCDF7F675F31B (author_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE testimonial ADD CONSTRAINT FK_E6BDCDF7A5BC2E0E FOREIGN KEY (trip_id) REFERENCES trip (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE testimonial ADD CONSTRAINT FK_E6BDCDF7F675F31B FOREIGN KEY (author_id) REFERENCES user (id)
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE testimonial DROP FOREIGN KEY FK_E6BDCDF7A5BC2E0E
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE testimonial DROP FOREIGN KEY FK_E6BDCDF7F675F31B
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE testimonial
+        SQL);
+    }
+}
